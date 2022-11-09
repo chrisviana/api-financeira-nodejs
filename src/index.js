606c6b35cd31908e9b3bd3f45be35fe302863df6
Christian Viana <christian.viana@acct.global>
2022-11-09 06:42:03 -0300

    Criado rota para realizar deposito

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { request } = require("express");
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 const app = express();
@@ -49,6 +50,23 @@ app.post("/account", (request, response) => {
 app.get("/statement/", verifyExistsAccountCPF , (request, response) => {
     const { customer } = request;
     return response.json(customer.statement)
+})
+
+app.post("/deposit", verifyExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    create_at: new Date(),
+    type: 'credit'
+  }
+
+  customer.statement.push(statementOperation)
+  
+  return response.status(201).send()
+
 })
 
 app.listen(3333);
